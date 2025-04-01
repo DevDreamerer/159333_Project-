739d91f27488331d8c61c5bb05a2390ee531fc22
DevDreamerer <3142019474@qq.com>
2025-04-01 13:07:31 +0800

    create database first

---
+++ Database_AgriHubNZ.sql
@@ -0,0 +1,207 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: AgriHubNZ
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `AgriHubNZ`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `AgriHubNZ` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `AgriHubNZ`;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `cost` float DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `create_time` datetime NOT NULL,
+  `update_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `productId` (`product_id`),
+  KEY `userId` (`user_id`),
+  CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=326 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_detail`
+--
+
+DROP TABLE IF EXISTS `order_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_detail` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `order_id` int NOT NULL COMMENT 'Order_PK',
+  `product_id` int NOT NULL COMMENT 'Product_PK',
+  `quantity` int NOT NULL COMMENT 'Quantity',
+  `cost` float NOT NULL COMMENT 'Cost',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `PK__EASYBUY___66E1BD8E2F10007B` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=178 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_detail`
+--
+
+LOCK TABLES `order_detail` WRITE;
+/*!40000 ALTER TABLE `order_detail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_detail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `user_id` int DEFAULT NULL COMMENT 'User_PK',
+  `login_name` varchar(255) DEFAULT NULL COMMENT 'Login_Name',
+  `user_address` varchar(255) DEFAULT NULL COMMENT 'User_Address',
+  `cost` float DEFAULT NULL COMMENT 'Total_Cost',
+  `serialnumber` varchar(255) DEFAULT NULL COMMENT 'Order_Number',
+  `create_time` datetime DEFAULT NULL COMMENT 'Create_Time',
+  `update_time` datetime NOT NULL COMMENT 'Update_Time',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=178 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `name` varchar(200) NOT NULL COMMENT 'Name',
+  `description` varchar(1024) DEFAULT NULL COMMENT 'Description',
+  `price` float NOT NULL COMMENT 'Price',
+  `stock` int NOT NULL COMMENT 'Stock',
+  `categorylevelone_id` int DEFAULT NULL COMMENT 'Category1',
+  `categoryleveltwo_id` int DEFAULT NULL COMMENT 'Category2',
+  `categorylevelthree_id` int DEFAULT NULL COMMENT 'Category3',
+  `file_name` varchar(200) DEFAULT NULL COMMENT 'File_Name',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `PK__EASYBUY___94F6E55132E0915F` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=777 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `product_category`
+--
+
+DROP TABLE IF EXISTS `product_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_category` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `name` varchar(20) NOT NULL COMMENT 'Name',
+  `parent_id` int NOT NULL COMMENT 'Parent_Id',
+  `type` int DEFAULT NULL COMMENT 'Level (1:Level 1 2:Level 2 3:Level 3)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `PK__EASYBUY___9EC2A4E236B12243` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=697 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `login_name` varchar(255) NOT NULL COMMENT 'Login_Name',
+  `user_name` varchar(255) NOT NULL COMMENT 'Use_Name',
+  `password` varchar(255) NOT NULL COMMENT 'Password',
+  `gender` int NOT NULL DEFAULT '1' COMMENT 'Sex (1: male 0: female)',
+  `email` varchar(80) DEFAULT NULL COMMENT 'Email',
+  `mobile` varchar(11) DEFAULT NULL COMMENT 'Mobile',
+  `file_name` varchar(255) DEFAULT NULL,
+  `create_time` datetime NOT NULL,
+  `update_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `PK__EASYBUY___C96109CC3A81B327` (`login_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `user_address`
+--
+
+DROP TABLE IF EXISTS `user_address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_address` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'PK_id',
+  `user_id` int DEFAULT NULL COMMENT 'User_PK',
+  `address` varchar(255) DEFAULT NULL COMMENT 'Address',
+  `remark` varchar(255) DEFAULT NULL COMMENT 'Remark',
+  `isdefault` int DEFAULT '0' COMMENT 'Whether it is the default address (1: yes 0 no)',
+  `create_time` datetime DEFAULT NULL COMMENT 'Create_Time',
+  `update_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
