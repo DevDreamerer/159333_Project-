c61cd55ceb956364f277f3a4d8ae5e65d935fe7b
Pettina <1353023196@qq.com>
2025-06-16 15:36:34 +0800

    update sql

--- script.sql
+++ script.sql
@@ -15,8 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
 CREATE DATABASE IF NOT EXISTS `nzhub`;
 USE `nzhub`;
+
 --
 -- Table structure for table `cart`
 --
@@ -58,11 +60,11 @@ DROP TABLE IF EXISTS `order_detail`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `order_detail` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `order_id` int NOT NULL COMMENT '订单主键',
-  `product_id` int NOT NULL COMMENT '商品主键',
-  `quantity` int NOT NULL COMMENT '数量',
-  `cost` float NOT NULL COMMENT '消费',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `cost` float NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `PK__EASYBUY___66E1BD8E2F10007B` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=189 DEFAULT CHARSET=utf8mb3;
@@ -82,18 +84,18 @@ UNLOCK TABLES;
 -- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `orders`; 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `user_id` int DEFAULT NULL COMMENT '用户主键',
-  `login_name` varchar(255) DEFAULT NULL COMMENT '用户名',
-  `user_address` varchar(255) DEFAULT NULL COMMENT '用户地址',
-  `cost` float DEFAULT NULL COMMENT '总金额',
-  `serialnumber` varchar(255) DEFAULT NULL COMMENT '订单号',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `login_name` varchar(255) DEFAULT NULL,
+  `user_address` varchar(255) DEFAULT NULL,
+  `cost` float DEFAULT NULL,
+  `serialnumber` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=185 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -139,7 +141,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` (`id`, `name`, `description`, `price`, `stock`, `categorylevelone_id`, `categoryleveltwo_id`, `categorylevelthree_id`, `file_name`, `origin`, `shelf_time`, `brand`, `rating`) VALUES (1,'Zespri SunGold Kiwifruit','Premium Golden Kiwifruit, Rich in Vitamin C',8.99,497,100,101,103,'kiwi.jpg','Bay of Plenty','2023-08-01 00:00:00','Zespri',4.8),(2,'Rockit Apples','Crisp and Sweet Mini Apples',4.5,294,100,101,104,'apple.jpg','Hawke\'s Bay','2023-08-05 00:00:00','Rockit',4.6),(3,'Lewis Road Organic Milk','Creamy Organic Full Cream Milk',6.99,200,200,201,203,'milk.jpg','Waikato','2023-07-20 00:00:00','Lewis Road',4.7),(4,'Cloudy Bay Sauvignon Blanc','Award-Winning Marlborough White Wine',29.99,100,300,301,303,'wine.jpg','Marlborough','2023-06-15 00:00:00','Cloudy Bay',4.9),(5,'Comvita Manuka Honey MGO 250+','Medical-Grade Manuka Honey',89.99,150,500,501,503,'manuka.jpg','Northland','2023-08-10 00:00:00','Comvita',4.8),(6,'Silver Fern Farms Lamb Leg','Grass-Fed Premium Lamb Leg',24.99,80,400,401,403,'lamb_leg.jpg','Canterbury','2023-08-12 00:00:00','Silver Fern',4.5),(7,'First Light Wagyu Ribeye Steak','Marbled Wagyu Beef Steak',39.99,50,400,401,404,'wagyu.jpg','Waikato','2023-08-15 00:00:00','First Light',4.7),(8,'Baby Spinach','Tender Organic Baby Spinach Leaves',5.99,198,100,102,107,'spinach.jpg','Pukekohe','2023-08-18 00:00:00','FreshCo',4.6),(9,'Free-Range Chicken Breast','Skinless Free-Range Chicken Breast',12.99,150,400,402,405,'chicken_breast.jpg','Waikato','2023-08-20 00:00:00','Bostock',4.7),(10,'Felton Road Pinot Noir','Central Otago Premium Pinot Noir',79.99,57,300,302,305,'pinot.jpg','Central Otago','2023-07-25 00:00:00','Felton Road',4.9),(11,'Mainland Tasty Cheddar','Aged Cheddar Cheese Block',8.5,120,200,202,205,'cheddar.jpg','Taranaki','2023-08-22 00:00:00','Mainland',4.5),(12,'Happy Hens Eggs','6-Pack Free-Range Eggs',6.99,300,400,402,406,'eggs.jpg','Canterbury','2023-08-15 00:00:00','Happy Hens',4.8),(13,'Esk Valley Cabernet Sauvignon','Hawke\'s Bay Bold Red Wine',45.99,70,300,302,306,'cabernet.jpg','Hawke\'s Bay','2023-08-15 00:00:00','Esk Valley',4.7),(14,'Whitestone Blue Cheese','Creamy Blue Vein Cheese',12.99,80,200,202,206,'blue_cheese.jpg','Oamaru','2023-08-25 00:00:00','Whitestone',4.6),(15,'Fresh Nelson Raspberries','Plump Juicy Raspberries',9.99,90,100,101,105,'raspberries.jpg','Nelson','2023-08-20 00:00:00','BerryFresh',4.5),(16,'Fresh Strawberries','Juicy Organic Strawberries',9.99,180,100,101,105,'strawberries.jpg','Nelson','2023-09-05 00:00:00','BerryHarvest',4.7),(17,'Organic Carrots','Sweet Crisp Root Vegetables',4.49,349,100,102,507,'carrots.jpg','Pukekohe','2023-09-01 00:00:00','EarthGoodness',4.6),(18,'Greek Style Yogurt','High-Protein Greek Yogurt',6.49,120,200,505,506,'greek_yogurt.jpg','Waikato','2023-09-10 00:00:00','PureDairy',4.8),(19,'Craft Pale Ale','Hoppy Nelson Sauvin IPA',14.99,80,300,508,509,'craft_beer.jpg','Nelson','2023-09-12 00:00:00','HopHaven',4.6),(20,'Free-Range Chicken Thighs','Skin-On Chicken Thighs',9.99,200,400,402,405,'chicken_thighs.jpg','Waikato','2023-09-15 00:00:00','Bostock',4.7),(21,'Lewis Road A2 Milk','Premium A2 Protein Whole Milk',7.99,150,200,201,204,'a2_milk.jpg','Waikato','2023-09-05 00:00:00','Lewis Road',4.7),(22,'Puhoi Valley Greek Yogurt','Creamy Traditional Greek Yogurt',5.99,200,200,505,506,'greek_puhoi.jpg','Northland','2023-09-10 00:00:00','Puhoi Valley',4.6),(23,'Mainland Vintage Cheddar','24-Month Aged Sharp Cheddar',10.99,80,200,202,205,'vintage_cheddar.jpg','Taranaki','2023-09-12 00:00:00','Mainland',4.8),(24,'Villa Maria Chardonnay','Rich Oak-Aged Marlborough Chardonnay',24.99,60,300,301,304,'chardonnay.jpg','Marlborough','2023-09-15 00:00:00','Villa Maria',4.7),(25,'Kumeu River Pinot Noir','Elegant Red Wine with Berry Notes',65.99,40,300,302,305,'kumeu_pinot.jpg','Auckland','2023-09-18 00:00:00','Kumeu River',4.9),(26,'Tuatara India Pale Ale','Hoppy Craft Beer with Citrus Finish',8.99,120,300,508,509,'tuatara_ipa.jpg','Wellington','2023-09-20 00:00:00','Tuatara',4.5),(27,'Airborne Clover Honey','Raw Unprocessed Clover Honey',12.99,180,500,501,504,'clover_honey.jpg','Canterbury','2023-09-22 00:00:00','Airborne',4.6),(28,'PureNZ Lavender Essential Oil','100% Pure Therapeutic Grade Oil',19.99,90,500,502,510,'lavender_oil.jpg','Nelson','2023-09-28 00:00:00','PureNZ',4.7),(29,'Nature\'s Way Tea Tree Oil','Antiseptic Skin Care Essential Oil',15.99,110,500,502,511,'teatree_oil.jpg','Bay of Plenty','2023-09-30 00:00:00','Nature\'s Way',4.5),(30,'Cricket Protein Powder','Sustainable Protein from Crickets (65% protein)',39.99,85,500,512,517,'cricket_protein.jpg','Auckland','2023-10-15 00:00:00','Sens',4.6),(31,'New Zealand Hemp Hearts','Raw Shelled Hemp Seeds (Rich in Omega-3)',22.99,120,500,512,513,'hemp_hearts.jpg','Canterbury','2023-10-12 00:00:00','The Brothers Green',4.8),(32,'Plant-Based Venison Patties','Wild Game Flavor from Pea Protein (0% Meat)',14.99,95,400,515,516,'plant_venison.jpg','Wellington','2023-10-20 00:00:00','Wild & Pure',4.7);
+INSERT INTO `product` (`id`, `name`, `description`, `price`, `stock`, `categorylevelone_id`, `categoryleveltwo_id`, `categorylevelthree_id`, `file_name`, `origin`, `shelf_time`, `brand`, `rating`) VALUES (1,'Zespri SunGold Kiwifruit','Premium Golden Kiwifruit, Rich in Vitamin C',8.99,497,100,101,103,'kiwi.jpg','Bay of Plenty','2023-08-01 00:00:00','Zespri',4.8),(2,'Rockit Apples','Crisp and Sweet Mini Apples',4.5,294,100,101,104,'apple.jpg','Hawke\'s Bay','2023-08-05 00:00:00','Rockit',4.6),(3,'Lewis Road Organic Milk','Creamy Organic Full Cream Milk',6.99,200,200,201,203,'milk.jpg','Waikato','2023-07-20 00:00:00','Lewis Road',4.7),(4,'Cloudy Bay Sauvignon Blanc','Award-Winning Marlborough White Wine',29.99,100,300,301,303,'wine.jpg','Marlborough','2023-06-15 00:00:00','Cloudy Bay',4.9),(5,'Comvita Manuka Honey MGO 250+','Medical-Grade Manuka Honey',89.99,150,500,501,503,'manuka.jpg','Northland','2023-08-10 00:00:00','Comvita',4.8),(6,'Silver Fern Farms Lamb Leg','Grass-Fed Premium Lamb Leg',24.99,80,400,401,403,'lamb_leg.jpg','Canterbury','2023-08-12 00:00:00','Silver Fern',4.5),(7,'First Light Wagyu Ribeye Steak','Marbled Wagyu Beef Steak',39.99,50,400,401,404,'wagyu.jpg','Waikato','2023-08-15 00:00:00','First Light',4.7),(8,'Baby Spinach','Tender Organic Baby Spinach Leaves',5.99,198,100,102,107,'spinach.jpg','Pukekohe','2023-08-18 00:00:00','FreshCo',4.6),(9,'Free-Range Chicken Breast','Skinless Free-Range Chicken Breast',12.99,150,400,402,405,'chicken_breast.jpg','Waikato','2023-08-20 00:00:00','Bostock',4.7),(10,'Felton Road Pinot Noir','Central Otago Premium Pinot Noir',79.99,57,300,302,305,'pinot.jpg','Central Otago','2023-07-25 00:00:00','Felton Road',4.9),(11,'Mainland Tasty Cheddar','Aged Cheddar Cheese Block',8.5,120,200,202,205,'cheddar.jpg','Taranaki','2023-08-22 00:00:00','Mainland',4.5),(12,'Happy Hens Eggs','6-Pack Free-Range Eggs',6.99,300,400,402,406,'eggs.jpg','Canterbury','2023-08-15 00:00:00','Happy Hens',4.8),(13,'Esk Valley Cabernet Sauvignon','Hawke\'s Bay Bold Red Wine',45.99,70,300,302,306,'cabernet.jpg','Hawke\'s Bay','2023-08-15 00:00:00','Esk Valley',4.7),(14,'Whitestone Blue Cheese','Creamy Blue Vein Cheese',12.99,80,200,202,206,'blue_cheese.jpg','Oamaru','2023-08-25 00:00:00','Whitestone',4.6),(15,'Fresh Nelson Raspberries','Plump Juicy Raspberries',9.99,90,100,101,105,'raspberries.jpg','Nelson','2023-08-20 00:00:00','BerryFresh',4.5),(16,'Fresh Strawberries','Juicy Organic Strawberries',9.99,180,100,101,105,'strawberries.jpg','Nelson','2023-09-05 00:00:00','BerryHarvest',4.7),(17,'Organic Carrots','Sweet Crisp Root Vegetables',4.49,349,100,102,507,'carrots.jpg','Pukekohe','2023-09-01 00:00:00','EarthGoodness',4.6),(18,'Greek Style Yogurt','High-Protein Greek Yogurt',6.49,120,200,505,506,'greek_yogurt.jpg','Waikato','2023-09-10 00:00:00','PureDairy',4.8),(19,'Craft Pale Ale','Hoppy Nelson Sauvin IPA',14.99,80,300,508,509,'craft_beer.jpg','Nelson','2023-09-12 00:00:00','HopHaven',4.6),(20,'Free-Range Chicken Thighs','Skin-On Chicken Thighs',9.99,200,400,402,405,'chicken_thighs.jpg','Waikato','2023-09-15 00:00:00','Bostock',4.7),(21,'Lewis Road A2 Milk','Premium A2 Protein Whole Milk',7.99,150,200,201,204,'a2_milk.jpg','Waikato','2023-09-05 00:00:00','Lewis Road',4.7),(22,'Puhoi Valley Greek Yogurt','Creamy Traditional Greek Yogurt',5.99,200,200,505,506,'greek_puhoi.jpg','Northland','2023-09-10 00:00:00','Puhoi Valley',4.6),(23,'Villa Maria Chardonnay','Rich Oak-Aged Marlborough Chardonnay',24.99,60,300,301,304,'chardonnay.jpg','Marlborough','2023-09-15 00:00:00','Villa Maria',4.7),(24,'Kumeu River Pinot Noir','Elegant Red Wine with Berry Notes',65.99,40,300,302,305,'kumeu_pinot.jpg','Auckland','2023-09-18 00:00:00','Kumeu River',4.9),(25,'Tuatara India Pale Ale','Hoppy Craft Beer with Citrus Finish',8.99,120,300,508,509,'tuatara_ipa.jpg','Wellington','2023-09-20 00:00:00','Tuatara',4.5),(26,'Airborne Clover Honey','Raw Unprocessed Clover Honey',12.99,180,500,501,504,'clover_honey.jpg','Canterbury','2023-09-22 00:00:00','Airborne',4.6),(27,'PureNZ Lavender Essential Oil','100% Pure Therapeutic Grade Oil',19.99,90,500,502,510,'lavender_oil.jpg','Nelson','2023-09-28 00:00:00','PureNZ',4.7),(28,'Nature\'s Way Tea Tree Oil','Antiseptic Skin Care Essential Oil',15.99,110,500,502,511,'teatree_oil.jpg','Bay of Plenty','2023-09-30 00:00:00','Nature\'s Way',4.5),(29,'Cricket Protein Powder','Sustainable Protein from Crickets (65% protein)',39.99,85,500,512,517,'cricket_protein.jpg','Auckland','2023-10-15 00:00:00','Sens',4.6),(30,'New Zealand Hemp Hearts','Raw Shelled Hemp Seeds (Rich in Omega-3)',22.99,120,500,512,513,'hemp_hearts.jpg','Canterbury','2023-10-12 00:00:00','The Brothers Green',4.8),(31,'Plant-Based Venison Patties','Wild Game Flavor from Pea Protein (0% Meat)',14.99,95,400,515,516,'plant_venison.jpg','Wellington','2023-10-20 00:00:00','Wild & Pure',4.7);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,9 +154,9 @@ DROP TABLE IF EXISTS `product_category`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product_category` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL COMMENT '分类名称',
-  `parent_id` int NOT NULL COMMENT '父分类ID',
-  `type` int DEFAULT NULL COMMENT '层级 (1:一级 2:二级 3:三级)',
+  `name` varchar(50) NOT NULL,
+  `parent_id` int NOT NULL,
+  `type` int DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=518 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -177,13 +179,13 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `login_name` varchar(255) NOT NULL COMMENT '登录名',
-  `user_name` varchar(255) NOT NULL COMMENT '用户名',
-  `password` varchar(255) NOT NULL COMMENT '密码',
-  `gender` int NOT NULL DEFAULT '1' COMMENT '性别(1:男 0：女)',
-  `email` varchar(80) DEFAULT NULL COMMENT '邮箱',
-  `mobile` varchar(11) DEFAULT NULL COMMENT '手机',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `login_name` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `gender` int NOT NULL DEFAULT '1',
+  `email` varchar(80) DEFAULT NULL,
+  `mobile` varchar(11) DEFAULT NULL,
   `file_name` varchar(255) DEFAULT NULL,
   `create_time` datetime NOT NULL,
   `update_time` datetime NOT NULL,
@@ -210,16 +212,17 @@ DROP TABLE IF EXISTS `user_address`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_address` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `user_id` int DEFAULT NULL COMMENT '用户主键',
-  `address` varchar(255) DEFAULT NULL COMMENT '地址',
-  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
-  `isdefault` int DEFAULT '0' COMMENT '是否是默认地址（1:是 0否）',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `isdefault` int DEFAULT '0',
+  `create_time` datetime DEFAULT NULL,
   `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `user_address`
@@ -244,4 +247,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-09 11:12:03
+-- Dump completed on 2025-06-15 17:42:05
